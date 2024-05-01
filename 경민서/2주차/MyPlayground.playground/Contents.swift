084f0a310f5fb42afac8a8dd64c876ccddb031fe
mxinseo <jade201913@gmail.com>
2024-05-01 20:28:19 +0900

    Feat: 4주차 Playground 생성

--- 경민서/2주차/MyPlayground.playground/Contents.swift
+++ 경민서/2주차/MyPlayground.playground/Contents.swift
@@ -1,21 +1,52 @@
-import UIKit
+import Foundation
 
-// Tuple
-var someTuple1 = (100, "kim", false)
-var someTuple2 = (score: 100, userName:"kim", isShowName: false)
-var someTuple3: (String, Int) = ("lee", 50)
+// Func Types
 
-// 조회
-someTuple2.2
-someTuple2.score
 
-let (score, name, isShowName) = someTuple1
-score
-name
-isShowName
+// () -> ()
+// () -> Void
+func sayHello() {
+    print("Hello")
+}
 
-// 업데이트
-someTuple2.score = 50
-someTuple2
 
-// 삭제, 추가 기능은 없음
+//(Int, Int) -> Int
+func plus (a: Int, b: Int) -> Int {
+    return a + b
+}
+
+func minus (a: Int, b: Int) -> Int {
+    return a - b
+}
+
+let input1 = 4
+let input2 = 5
+var result = 0
+
+func selectButton1(buttonStyle: String) {
+    if buttonStyle == "+" {
+        result = plus(a: input1, b: input2)
+    } else if buttonStyle == "-" {
+        result = minus(a: input1, b: input2)
+    }
+}
+
+// 선택만하고 싶은데, 계산과 진행까지 더 진행되어 버림, 더 복잡해져 보임
+// 이름에 충실한 기능만 만들고 싶다면
+
+// 초기값이 없으므로 옵셔널로 지정
+var calc: ((Int, Int) -> Int)?
+
+// 실행이 아니라 기능만 담고 있는 것임. 작동 준비만 함
+func selectButton2(buttonStyle: String) {
+    if buttonStyle == "+" {
+        calc = plus
+    } else if buttonStyle == "-" {
+        calc = minus
+    }
+}
+
+// 선택한 연산 실행
+func showResult() {
+    calc?(input1, input2)
+}
