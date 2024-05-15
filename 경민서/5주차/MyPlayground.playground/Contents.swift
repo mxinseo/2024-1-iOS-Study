f7fbd4ce8e62768726cfbee51391009e92cbe395
mxinseo <jade201913@gmail.com>
2024-05-15 23:00:14 +0900

    Feat: 1-35. property_2

--- 경민서/5주차/MyPlayground.playground/Contents.swift
+++ 경민서/5주차/MyPlayground.playground/Contents.swift
@@ -1,58 +1,20 @@
 import Foundation
 
+// property observer
 
-// property
-
-var name = ""
-
-class MyClass {
-    var name = ""
-}
-
-struct MyStruct {
-    var name = ""
-}
-
-let myClass = MyClass()
-myClass.name = "kim"
-
-
-let myStruct = MyStruct()
-//myStruct.name = "kim". // error
-
-
-// lazy stored property
-class MyClass2{
-    lazy var name = ""
-    var age = 20
-    lazy var isAdult = age > 19 ? true : false
-    lazy var names = ["kim", "lee"]
-    
-//    lazy var nameCount = names.count // logical error
-    func nameCunt2() -> Int {
-        return name.count
-    }
-}
-
-var myClass2 = MyClass2()
-
-// computed property
-class MyProfile {
-    var age = 20
-    
-    // computed property, func처럼 작동
-    var isAdult: Bool {
-        return age > 19 ? true : false
-    }
-    
-    var isAdult3: Bool {
-        get {
-            !tempIsAdult
+class StepCounter {
+    var steps = 0 {
+        willSet {
+            print("현재값", self.steps)
+            print("설정하려는 값", newValue)
         }
-        set {
-            tempIsAdult = newValue // 새로운 값이 들어오는 순간 tempIsAdult에 저장해줌
+        didSet {
+            print("이전 값", oldValue)
+            print("현재 값", steps)
         }
     }
-    
-    var tempIsAdult = false
 }
+
+let stepCounter = StepCounter()
+stepCounter.steps = 10
+
