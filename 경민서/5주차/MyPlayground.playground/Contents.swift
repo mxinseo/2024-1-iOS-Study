262a33e1ccd76cc898465b010231fa0d6a549005
mxinseo <jade201913@gmail.com>
2024-05-15 23:13:07 +0900

    Feat:  1-36. property_3

--- 경민서/5주차/MyPlayground.playground/Contents.swift
+++ 경민서/5주차/MyPlayground.playground/Contents.swift
@@ -1,20 +1,45 @@
 import Foundation
 
-// property observer
+// property wrappers
+// 프로퍼티에 기능을 넣는다
 
-class StepCounter {
-    var steps = 0 {
-        willSet {
-            print("현재값", self.steps)
-            print("설정하려는 값", newValue)
+//글자 입력 제한 - 10
+@propertyWrapper
+struct CharLimit {
+    var tempText = ""
+    
+    var limitCount = 0
+    
+    var wrappedValue: String {
+        get {
+            return tempText
         }
-        didSet {
-            print("이전 값", oldValue)
-            print("현재 값", steps)
+        set {
+            var myText = newValue
+            while myText.count > limitCount {
+                myText.removeLast()
+            }
+            tempText = myText
         }
+    }
+    
+    init(wrappedValue: String, limitCount: Int) {
+        self.wrappedValue = wrappedValue
+        self.limitCount = limitCount
     }
 }
 
-let stepCounter = StepCounter()
-stepCounter.steps = 10
+class Display {
+    @CharLimit(limitCount: 10)
+    var name: String = ""
+    
+    @CharLimit(limitCount: 5)
+    var password: String = ""
+}
 
+let display = Display()
+display.name = "1232344qwe34"
+display.password = "1231342412"
+
+display.name
+display.password
