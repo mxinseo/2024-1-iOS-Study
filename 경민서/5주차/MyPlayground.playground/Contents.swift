897e64c58c16c7de0294a9c027fbc3565866e8b4
mxinseo <jade201913@gmail.com>
2024-05-15 23:30:47 +0900

    Feat : 1-37. subscript

--- 경민서/5주차/MyPlayground.playground/Contents.swift
+++ 경민서/5주차/MyPlayground.playground/Contents.swift
@@ -1,45 +1,67 @@
 import Foundation
 
-// property wrappers
-// 프로퍼티에 기능을 넣는다
+// Subscript
 
-//글자 입력 제한 - 10
-@propertyWrapper
-struct CharLimit {
-    var tempText = ""
+var names = ["lee", "kim"]
+names[0]
+names[1]
+
+var colorValue = ["red": "100", "blue": "001"]
+colorValue["red"] = "빨강"
+
+struct Member {
+    var name:String
+    var age: Int
+}
+
+let member1 = Member(name: "kim", age: 20)
+let member2 = Member(name: "lee", age: 25)
+let member3 = Member(name: "mon", age: 22)
+let member4 = Member(name: "seo", age: 32)
+
+let members = [member1, member2, member3, member4]
+
+class Group {
+    var mambers = [Member]()
     
-    var limitCount = 0
+    // 데이터에 []로 바로 접근한다.
+    subscript(index: Int) -> Member {
+        return mambers[index]
+    }
+}
+
+let group = Group()
+group.mambers = [member1, member2, member3, member4]
+group[0].name
+
+
+// 나이대로 데이터 가져오기
+class Group2 {
     
-    var wrappedValue: String {
+    enum AgeType {
+        case _20s
+        case _30s
+    }
+    
+    var members = [Member]()
+    
+    // 데이터에 []로 바로 접근한다.
+    subscript(ageGroup: AgeType) -> [Member] {
         get {
-            return tempText
-        }
-        set {
-            var myText = newValue
-            while myText.count > limitCount {
-                myText.removeLast()
+            var outputMembers = [Member]()
+            for member in members {
+                if ageGroup == ._20s && 20..<30 ~= member.age {
+                    outputMembers.append(member)
+                }
+                if ageGroup == ._30s && 30..<40 ~= member.age {
+                    outputMembers.append(member)
+                }
             }
-            tempText = myText
+            return outputMembers
         }
     }
-    
-    init(wrappedValue: String, limitCount: Int) {
-        self.wrappedValue = wrappedValue
-        self.limitCount = limitCount
-    }
 }
 
-class Display {
-    @CharLimit(limitCount: 10)
-    var name: String = ""
-    
-    @CharLimit(limitCount: 5)
-    var password: String = ""
-}
-
-let display = Display()
-display.name = "1232344qwe34"
-display.password = "1231342412"
-
-display.name
-display.password
+let group2 = Group2()
+group2.members = [member1, member2, member3, member4]
+group2[._20s]
