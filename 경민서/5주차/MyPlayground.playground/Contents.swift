363d1957c9887c52a655001c24f6b4f8632cd84d
mxinseo <jade201913@gmail.com>
2024-05-15 22:27:43 +0900

    Feat: 1-33. static

--- 경민서/5주차/MyPlayground.playground/Contents.swift
+++ 경민서/5주차/MyPlayground.playground/Contents.swift
@@ -1,59 +1,35 @@
 import Foundation
 
-// Deinitializer 초기화 해제
+// static
+// 하나의 원본, 똑같은게 여러개 생기지 않는 옵션
 
-class Test {
-    init() {
-        print("init")
-    }
-    
-    deinit{
-        // 클래스가 메모리에서 해제 되었을 때
-        print("deinit")
-    }
-}
-
-// reference count == 0 인 시점에
-// 메모리에서 해제
-
-var test1: Test? = Test()
-var test2: Test? = test1
-var test3: Test? = test1
-
-test1 = nil
-test2 = nil
-test3 = nil
+// type property
+// type method
 
 class Bank {
-    static var coin: Int = 500
+    static var coin: Int = 0
 }
-
-class Player {
-    var coin:Int
-    init(coin: Int) {
-        self.coin = coin
-    }
-    
-    func plus() {
-        self.coin += 100
-    }
-    
-    deinit {
-        Bank.coin += self.coin
-    }
-}
-
-var player1: Player? = Player(coin: 100)
-var player2: Player? = Player(coin: 100)
-
-player1?.plus()
-player1?.plus()
-
-player2?.plus()
 
 Bank.coin
+Bank.coin=100
+Bank.coin
 
-player1 = nil
-player2 = nil
+class Player {
+    func plus() {
+        Bank.coin += 1
+    }
+    
+    func minus() {
+        Bank.coin -= 1
+    }
+}
+
+let player1 = Player()
+let player2 = Player()
+let player3 = Player()
+
+player1.plus()
+player2.plus()
+player3.plus()
 
 Bank.coin
