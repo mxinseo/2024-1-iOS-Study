e396bd9088bf6b19d9213310a58da96bf6c937e3
mxinseo <jade201913@gmail.com>
2024-05-15 22:17:58 +0900

    feat: 1-32. deinit

--- 경민서/5주차/MyPlayground.playground/Contents.swift
+++ 경민서/5주차/MyPlayground.playground/Contents.swift
@@ -1,116 +1,59 @@
 import Foundation
 
-// class and struct
+// Deinitializer 초기화 해제
 
-// 1 oz = 29.5ml
-// 1ml = 0.033oz
-
-//customizing initialization
-struct UnitConversion {
-    var oz: Double
-    var ml: Double
-    
-    init(oz: Double) {
-        self.oz = oz
-        self.ml = oz * 29.5
-    }
-    
-    init(ml: Double) {
-        self.ml = ml
-        self.oz = ml / 29.5
-    }
-}
-
-var unitConversion = UnitConversion(ml: 1)
-unitConversion.ml
-unitConversion.oz
-
-var unitConversion2 = UnitConversion(oz: 1)
-unitConversion2.ml
-unitConversion2.oz
-
-// - convenience initailizer
-// - 조건 → 내부에서 다른 init을 반드시 실행
-
-class Profile{
-    var name : String
-    var age: Int
-    var isAdult: Bool
-    
-    // Designated initializer
-    init(name: String, age:Int) {
-        self.name = name
-        self.age = age
-        self.isAdult = age > 19 ? true: false
-    }
-    
-//    init(age:Int) {
-//        self.name = "" // 빈 스트링
-//        self.age = age
-//        self.isAdult = age > 19 ? true: false
-//    }
-    
-    convenience init(age: Int) {
-        self.init(name: "", age: age)
-    }
-    
-    func showName() {
-        print(name)
-    }
-    
-}
-
-// 상속
-class MemberProfile: Profile {
-    override init(name: String, age: Int) {
-        let newName = name + "님"
-        super.init(name: newName, age: age)
-    }
-    
-    override func showName() {
-        print(name + "하이")
-    }
-}
-
-
-let member = MemberProfile(name: "kim", age: 20)
-member.showName()
-
-
-// Required init
-// 상속
-class BaseView {
-    init(frame: CGRect) {
-        
-    }
-    
-    required init(coder: NSCoder) {
-        
-    }
-}
-
-class RedView: BaseView {
+class Test {
     init() {
-        let frame = CGRect()
-        super.init(frame: frame)
+        print("init")
     }
-    override init(frame: CGRect) {
-        let frame = CGRect()
-        super.init(frame: frame)
-    }
-    required init(coder: NSCoder) {
-        super.init(coder: coder)
+    
+    deinit{
+        // 클래스가 메모리에서 해제 되었을 때
+        print("deinit")
     }
 }
 
-// Failable initializer
-//init?
+// reference count == 0 인 시점에
+// 메모리에서 해제
 
-class MyClass {
-    init?(type: String) {
-        if type == "" {
-            return nil
-        }
-        print("success", type)
+var test1: Test? = Test()
+var test2: Test? = test1
+var test3: Test? = test1
+
+test1 = nil
+test2 = nil
+test3 = nil
+
+class Bank {
+    static var coin: Int = 500
+}
+
+class Player {
+    var coin:Int
+    init(coin: Int) {
+        self.coin = coin
+    }
+    
+    func plus() {
+        self.coin += 100
+    }
+    
+    deinit {
+        Bank.coin += self.coin
     }
 }
+
+var player1: Player? = Player(coin: 100)
+var player2: Player? = Player(coin: 100)
+
+player1?.plus()
+player1?.plus()
+
+player2?.plus()
+
+Bank.coin
+
+player1 = nil
+player2 = nil
+
+Bank.coin
